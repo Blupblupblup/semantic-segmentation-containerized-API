c3d9c0f082817a47a05792c112d2f4b791613705
Blupblupblup <24482549+Blupblupblup@users.noreply.github.com>
2025-09-28 23:06:22 +0200

    Explicit testing methods names.

--- load_testing.py
+++ load_testing.py
@@ -4,13 +4,13 @@ class PixelCounterUser(HttpUser):
     wait_time = between(0.5, 3)
 
     @task
-    def count_cat_pixels(self):
+    def count_cat_pixels_1query(self):
         payload = {"image_url": "http://images.cocodataset.org/val2017/000000039769.jpg"}
         headers = {"Content-Type": "application/json"}
         self.client.post("/count_cat_pixels", json=payload, headers=headers)
 
     @task
-    def count_cat_pixels(self):
+    def count_cat_pixels_4queries(self):
         payload = {"image_url": "https://farm2.staticflickr.com/1326/5164178695_2c03ab4d4d_z.jpg"}
         headers = {"Content-Type": "application/json"}
         self.client.post("/count_cat_pixels", json=payload, headers=headers)
